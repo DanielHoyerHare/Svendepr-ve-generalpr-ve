21df09a1d28ab430a50a0ca39dc93bc62718a50f
LoQhu <nick4453@elev.tec.dk>
2024-05-30 10:58:03 +0200

    Add project files.

---
+++ Calorie_Tracker/Calorie_Tracker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Calorie_Tracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Calorie_Tracker/Calorie_Tracker/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Calorie_Tracker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
