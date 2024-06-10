19d0ae3adde533ebedd7425590d924d387f742f8
Abdey Habdey <andreaszzz51@gmail.com>
2024-06-10 09:26:03 +0200

    Massere bindings i registers

---
+++ Calorie_Tracker/Calorie_Tracker/Models/ApiModels/Goal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Calorie_Tracker.Models.ApiModels;
+
+class Goal
+{
+    [JsonPropertyName("startDate")]
+    public DateTime? StartDate { get; set; }  
+
+    [JsonPropertyName("calorieGoal")]
+    public double? CalorieGoal { get; set; }  
+
+    [JsonPropertyName("carbonhydratesGoal")]
+    public double? CarbohydratesGoal { get; set; } 
+
+    [JsonPropertyName("proteinGoal")]
+    public double? ProteinGoal { get; set; }  
+
+    [JsonPropertyName("fatGoal")]
+    public double? FatGoal { get; set; }  
+
+    [JsonPropertyName("user")]
+    public string UserId { get; set; }  
+}
--- Calorie_Tracker/Calorie_Tracker/Services/ApiService.cs
+++ Calorie_Tracker/Calorie_Tracker/Services/ApiService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Calorie_Tracker.Services
@@ -20,7 +21,7 @@ namespace Calorie_Tracker.Services
 
         public async Task<Bruger> getBrugerInformation(int id)
         {
-            if(Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
             {
                 return null;
             }
@@ -28,5 +29,39 @@ namespace Calorie_Tracker.Services
             return await _httpClient.GetFromJsonAsync<Bruger>($"query={id}");
         }
 
+        public async Task<bool> RegisterUserAsync(Bruger bruger)
+        {
+            var json = JsonSerializer.Serialize(bruger);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("register", content);
+
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public async Task<bool> RegisterGoalAsync(Goal goal)
+        {
+            var json = JsonSerializer.Serialize(goal);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("register", content);
+
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
