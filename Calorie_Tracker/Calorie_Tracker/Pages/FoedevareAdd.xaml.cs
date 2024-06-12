aa17ceb7f73e6d25929042d0c8bafb96a866c2c1
Abdey Habdey <andreaszzz51@gmail.com>
2024-06-12 12:19:15 +0200

    FoedeVare

---
+++ Calorie_Tracker/Calorie_Tracker/Pages/FoedevareAdd.xaml.cs
@@ -0,0 +1,12 @@
+using Calorie_Tracker.ViewModel;
+
+namespace Calorie_Tracker.Pages;
+
+public partial class FoedevareAdd : ContentPage
+{
+	public FoedevareAdd(FoedeVareAddView vm)
+	{
+		InitializeComponent();
+		BindingContext = vm;
+	}
+}
--- Calorie_Tracker/Calorie_Tracker/Services/ApiService.cs
+++ Calorie_Tracker/Calorie_Tracker/Services/ApiService.cs
@@ -1,8 +1,10 @@
 ﻿using Calorie_Tracker.Models.ApiModels;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
@@ -42,9 +44,24 @@ namespace Calorie_Tracker.Services
             }
         }
 
+        public async Task<ObservableCollection<DailyIntake>> getDailyIntakeAsync(int id)
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<ObservableCollection<DailyIntake>>($"query={id}");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+
         public async Task<bool> RegisterUserAsync(Bruger bruger)
         {
-            var json = JsonSerializer.Serialize(bruger);
+            var json = System.Text.Json.JsonSerializer.Serialize(bruger);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
             var response = await _httpClient.PostAsync("register", content);
@@ -59,9 +76,45 @@ namespace Calorie_Tracker.Services
                 return false;
             }
         }
+
+        public async Task<string> LoginAsync(string email, string password)
+        {
+            var user = new
+            {
+                email = email.Contains("@") ? email : null,
+                password
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("login", content);
+
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                return result;
+            }
+            return null;
+
+        }
+
+        public async Task<Bruger> GetUserInfoAsync(string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.GetAsync("user");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<Bruger>();
+            }
+
+            return null;
+        }
+
+
         public async Task<bool> RegisterGoalAsync(Goal goal)
         {
-            var json = JsonSerializer.Serialize(goal);
+            var json = System.Text.Json.JsonSerializer.Serialize(goal);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
             var response = await _httpClient.PostAsync("register", content);
